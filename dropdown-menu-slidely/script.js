0711a9ad148ca12a638e4a4d4762bb141cdfe5b5
Masoud <masoudk1466@gmail.com>
2022-01-24 23:32:58 +0330

    Update and check

--- dropdown-menu-slidely/script.js
+++ dropdown-menu-slidely/script.js
@@ -8,27 +8,28 @@ const setting_item = document.querySelector('.setting-item');
 const help_item = document.querySelector('.help-item');
 const setting_btn = document.querySelector('.back-setting-btn');
 const help_btn = document.querySelector('.back-help-btn');
+
 drop_btn.onclick = () => {
-	menu_wrapper.classList.toggle('show');
-	tooltip.classList.toggle('show');
+    menu_wrapper.classList.toggle('show');
+    tooltip.classList.toggle('show');
 };
 setting_item.onclick = () => {
-	menu_bar.style.marginLeft = '-400px';
-	setTimeout(() => {
-		setting_drop.style.display = 'block';
-	}, 100);
+    menu_bar.style.marginLeft = '-400px';
+    setTimeout(() => {
+        setting_drop.style.display = 'block';
+    }, 100);
 };
 help_item.onclick = () => {
-	menu_bar.style.marginLeft = '-400px';
-	setTimeout(() => {
-		help_drop.style.display = 'block';
-	}, 100);
+    menu_bar.style.marginLeft = '-400px';
+    setTimeout(() => {
+        help_drop.style.display = 'block';
+    }, 100);
 };
 setting_btn.onclick = () => {
-	menu_bar.style.marginLeft = '0px';
-	setting_drop.style.display = 'none';
+    menu_bar.style.marginLeft = '0px';
+    setting_drop.style.display = 'none';
 };
 help_btn.onclick = () => {
-	help_drop.style.display = 'none';
-	menu_bar.style.marginLeft = '0px';
+    help_drop.style.display = 'none';
+    menu_bar.style.marginLeft = '0px';
 };
