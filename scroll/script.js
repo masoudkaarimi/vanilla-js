de6ffdc1ef8ee42aabb2a92b2dce391160db8d85
Masoud <masoudk1466@gmail.com>
2022-04-25 23:30:03 +0430

    Nav toggler function added

---
+++ scroll/script.js
@@ -0,0 +1,27 @@
+// Element.getBoundingClientRect() method returns the size of an element and its position relative to the viewport.
+// pageYOffset is a read - only window property that returns the number of pixels the document has been scrolled vertically.
+// slice extracts a section of a string without modifying original string
+//offsetTop - A Number, representing the top position of the element, in pixels
+
+// ========== Set date ==========
+// Select span
+const date = document.getElementById("date");
+date.innerHTML = new Date().getFullYear();
+
+// ========== Close links ==========
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+  // linksContainer.classList.toggle("show-links");
+  // console.log(linksContainer.getBoundingClientRect());
+
+  const linksHeight = links.getBoundingClientRect().height;
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  if (containerHeight === 0) {
+    linksContainer.style.height = `${linksHeight}px`;
+  } else {
+    linksContainer.style.height = 0;
+  }
+});
