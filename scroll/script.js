998a54e9a27508055a4aedc27ba6f2c3c21e7a33
Masoud <masoudk1466@gmail.com>
2022-04-25 23:33:08 +0430

    Fixed navbar added

--- scroll/script.js
+++ scroll/script.js
@@ -13,7 +13,7 @@ const navToggle = document.querySelector(".nav-toggle");
 const linksContainer = document.querySelector(".links-container");
 const links = document.querySelector(".links");
 
-navToggle.addEventListener("click", function () {
+navToggle.addEventListener("click", () => {
   // linksContainer.classList.toggle("show-links");
   // console.log(linksContainer.getBoundingClientRect());
 
@@ -23,5 +23,26 @@ navToggle.addEventListener("click", function () {
     linksContainer.style.height = `${linksHeight}px`;
   } else {
     linksContainer.style.height = 0;
+  }
+});
+
+// ========== Fixed navbar ==========
+const navbar = document.getElementById("nav");
+const topLink = document.querySelector(".top-link");
+
+window.addEventListener("scroll", () => {
+  const scrollHeight = window.pageYOffset;
+  const navHeight = navbar.getBoundingClientRect().height;
+  if (scrollHeight > navHeight) {
+    navbar.classList.add("fixed-nav");
+  } else {
+    navbar.classList.remove("fixed-nav");
+  }
+
+  // Setup back to top link
+  if (scrollHeight > 500) {
+    topLink.classList.add("show-link");
+  } else {
+    topLink.classList.remove("show-link");
   }
 });
