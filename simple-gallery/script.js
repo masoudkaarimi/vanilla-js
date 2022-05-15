6cef11afd75f8b95d6ab4b1cdece77805b9a20a5
Masoud <masoudk1466@gmail.com>
2022-05-15 13:29:34 +0430

    Class gallery created

--- simple-gallery/script.js
+++ simple-gallery/script.js
@@ -106,3 +106,39 @@ PrototypeGallery.prototype.chooseImage = function (e) {
 };
 
 const nature = new PrototypeGallery(getElement('.nature'));
+
+// ==================== Class Gallery ====================
+class ClassGallery {
+	constructor(element) {
+		this.container = element;
+		this.list = [...element.querySelectorAll('.img')];
+
+		// Targets
+		this.modal = getElement('.modal');
+		this.modalImg = getElement('.main-img');
+		this.imageName = getElement('.image-name');
+		this.modalImages = getElement('.modal-images');
+		this.closeBtn = getElement('.close-btn');
+		this.nextBtn = getElement('.next-btn');
+		this.prevBtn = getElement('.prev-btn');
+
+		// Binding this to methods
+		this.closeModal = this.closeModal.bind(this);
+		this.nextImage = this.nextImage.bind(this);
+		this.prevImage = this.prevImage.bind(this);
+		this.chooseImage = this.chooseImage.bind(this);
+
+		// Container event
+		this.container.addEventListener(
+			'click',
+			function (e) {
+				if (e.target.classList.contains('img')) {
+					this.openModal(e.target, this.list);
+				}
+			}.bind(this)
+		);
+	}
+
+}
+
+const city = new ClassGallery(getElement('.city'));
